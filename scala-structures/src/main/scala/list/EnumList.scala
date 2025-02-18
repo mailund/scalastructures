1784694f3f7b837395fba969aaab17bbc1a4c195
Thomas Mailund <tm@kvantify.dk>
2025-02-18 15:24:42 +0100

    Fixed EnumList

--- scala-structures/src/main/scala/list/EnumList.scala
+++ scala-structures/src/main/scala/list/EnumList.scala
@@ -1,6 +1,5 @@
 package list
 
-/*
 enum EnumList[+T] extends List[T]:
   case Empty
   case Cons(h: T, t: EnumList[T])
@@ -28,119 +27,8 @@ enum EnumList[+T] extends List[T]:
     case Empty         => acc
     case Cons(h, tail) => tail.rev(Cons(h, acc))
 
-  val reverse = rev(Empty)
+  def reverse: EnumList[T] = rev(Empty)
 
 object EnumList extends ListCompanion[EnumList] {
   override def empty[T]: EnumList[T] = Empty
 }
-
-=> java.lang.ExceptionInInitializerError
-
- */
-
-/*
-sealed abstract class EnumList[+T] extends List[T]:
-  def isEmpty: Boolean
-  def head: T
-  def tail: EnumList[T]
-  val reverse: EnumList[T] = rev(Empty)(this)
-
-  def prepend[U >: T](elem: U): EnumList[U] = Cons(elem, this)
-  def append[U >: T](elem: U): EnumList[U] = this match
-    case Empty         => Cons(elem, Empty)
-    case Cons(h, tail) => Cons(h, tail.append(elem))
-
-
-case object Empty extends EnumList[Nothing]:
-  def isEmpty: Boolean = true
-  def head: Nothing = throw new NoSuchElementException("head of empty list")
-  def tail: EnumList[Nothing] = throw new UnsupportedOperationException(
-    "tail of empty list"
-  )
-
-case class Cons[+T](h: T, t: EnumList[T]) extends EnumList[T]:
-  def isEmpty: Boolean = false
-  def head: T = h
-  def tail: EnumList[T] = t
-
-def rev[T](acc: EnumList[T])(list: EnumList[T]): EnumList[T] = list match
-  case Empty         => acc
-  case Cons(h, tail) => rev(Cons(h, acc))(tail)
-
-object EnumList extends ListCompanion[EnumList]:
-  def empty[T]: EnumList[T] = Empty
-
-  => java.lang.ExceptionInInitializerError
-
- */
-
-/*
-sealed abstract class EnumList[+T] extends List[T]:
-  def isEmpty: Boolean
-  def head: T
-  def tail: EnumList[T]
-
-  def prepend[U >: T](elem: U): EnumList[U] = Cons(elem, this)
-  def append[U >: T](elem: U): EnumList[U] = this match
-    case Empty         => Cons(elem, Empty)
-    case Cons(h, tail) => Cons(h, tail.append(elem))
-
-  def reverse: List[T] =
-    var acc: EnumList[T] = Empty
-    var lst = this
-    while !lst.isEmpty do
-      acc = acc.prepend(lst.head)
-      lst = lst.tail
-    acc
-
-case object Empty extends EnumList[Nothing]:
-  def isEmpty: Boolean = true
-  def head: Nothing = throw new NoSuchElementException("head of empty list")
-  def tail: EnumList[Nothing] = throw new UnsupportedOperationException(
-    "tail of empty list"
-  )
-
-case class Cons[+T](h: T, t: EnumList[T]) extends EnumList[T]:
-  def isEmpty: Boolean = false
-  def head: T = h
-  def tail: EnumList[T] = t
-
-object EnumList extends ListCompanion[EnumList]:
-  def empty[T]: EnumList[T] = Empty
-
-  // Works, but not pure FP
-
- */
-
-sealed abstract class EnumList[+T] extends List[T]:
-  def isEmpty: Boolean
-  def head: T
-  def tail: EnumList[T]
-
-  def prepend[U >: T](elem: U): EnumList[U] = Cons(elem, this)
-  def append[U >: T](elem: U): EnumList[U] = this match
-    case Empty         => Cons(elem, Empty)
-    case Cons(h, tail) => Cons(h, tail.append(elem))
-
-  def reverse: List[T] =
-    var acc: EnumList[T] = Empty
-    var lst = this
-    while !lst.isEmpty do
-      acc = acc.prepend(lst.head)
-      lst = lst.tail
-    acc
-
-case object Empty extends EnumList[Nothing]:
-  def isEmpty: Boolean = true
-  def head: Nothing = throw new NoSuchElementException("head of empty list")
-  def tail: EnumList[Nothing] = throw new UnsupportedOperationException(
-    "tail of empty list"
-  )
-
-case class Cons[+T](h: T, t: EnumList[T]) extends EnumList[T]:
-  def isEmpty: Boolean = false
-  def head: T = h
-  def tail: EnumList[T] = t
-
-object EnumList extends ListCompanion[EnumList]:
-  def empty[T]: EnumList[T] = Empty
